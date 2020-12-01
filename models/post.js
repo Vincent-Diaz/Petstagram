342cacb8449a6b8b781047856b26eb8c0c82842d
Dung Nguyen <dudu.nt107@gmail.com>
2020-12-01 11:02:12 -0600

    created user model and updated post model

--- models/post.js
+++ models/post.js
@@ -1,13 +1,11 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const postSchema = new Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  synopsis: String,
-  date: { type: Date, default: Date.now }
+const PostSchema = new Schema({
+  text: { type: String, required: true },
+  postCreated: { type: Date, default: Date.now }
 });
 
-const Post = mongoose.model("post", postSchema);
+const Post = mongoose.model("Post", PostSchema);
 
 module.exports = Post;
