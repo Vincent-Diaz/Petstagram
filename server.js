87661843c8d11f3f072a4af9b4e924a8607ad72b
Vincent Diaz <Vincent-Diaz@users.noreply.github.com>
2020-12-02 13:16:54 -0600

    added express session

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const session = require("express-session");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
