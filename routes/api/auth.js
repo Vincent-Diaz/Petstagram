4da45816eae26497c1e5092a48cec75ab000121e
Dung Nguyen <dudu.nt107@gmail.com>
2020-12-03 11:13:45 -0600

    updated

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,49 +1,52 @@
 // Requiring our models and passport as we've configured it
-const db = require("../../models");
-const passport = require("../../config/passport");
-const router = require('express').Router;
+const db = require('../../models');
+const passport = require('../../config/passport');
+const router = require('express').Router();
 
-module.exports = function() {
-  // Route for logging in
-  router.post("/api/login", passport.authenticate("local"), function(req, res) {
-    res.json(req.user);
-  });
+// module.exports = function (app) {
+// console.log({ router });
+// Route for logging in
 
-  // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
-  // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
-  // otherwise send back an error
-  router.post("/api/signup", function(req, res) {
-    db.User.create({
-      userName: req.body.userName,
-      email: req.body.email,
-      password: req.body.password
+router.post('/login', passport.authenticate('local'), function (req, res) {
+  res.json(req.user);
+});
+
+router.post('/signup', function (req, res) {
+  console.log("req.body", req.body)
+  db.User.create({
+    userName: req.body.userName,
+    email: req.body.email,
+    password: req.body.password,
+  })
+    .then(user =>res.json(user))
+    .then(function () {
+      res.redirect(307, '/api/login');
     })
-      .then(function() {
-        res.redirect(307, "/api/login");
-      })
-      .catch(function(err) {
-        res.status(401).json(err);
-      });
-  });
+    .catch(function (err) {
+      res.status(401).json(err);
+    });
+});
 
-  // Route for logging user out
-  router.get("/logout", function(req, res) {
-    req.logout();
-    res.redirect("/");
-  });
+// Route for logging user out
+router.get('/logout', function (req, res) {
+  req.logout();
+  res.redirect('/');
+});
 
-  // Route for getting some data about our user to be used client side
-  router.get("/api/user_data", function(req, res) {
-    if (!req.user) {
-      // The user is not logged in, send back an empty object
-      res.status(401).json({});
-    } else {
-      // Otherwise send back the user's email and id
-      // Sending back a password, even a hashed password, isn't a good idea
-      res.json({
-        email: req.user.email,
-        id: req.user.id
-      });
-    }
-  });
-};
+// Route for getting some data about our user to be used client side
+router.get('/api/user_data', function (req, res) {
+  console.log(req.user)
+  if (!req.user) {
+    // The user is not logged in, send back an empty object
+    res.status(401).json({});
+  } else {
+    // Otherwise send back the user's email and id
+    // Sending back a password, even a hashed password, isn't a good idea
+    res.json({
+      email: req.user.email,
+      id: req.user.id,
+    });
+  }
+});
+
+module.exports = router;
