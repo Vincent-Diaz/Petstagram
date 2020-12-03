a529c8486293562e68a94eece417e416f477dc62
Dung Nguyen <dudu.nt107@gmail.com>
2020-12-02 21:33:42 -0600

    added passport middleware to use authentication

---
+++ config/passport.js
@@ -0,0 +1,54 @@
+const passport = require("passport");
+const bcrypt = require("bcryptjs");
+const { User } = require("../models");
+const LocalStrategy = require("passport-local").Strategy;
+
+passport.use(
+    new LocalStrategy({usernameField:"email"}, (email, password,done) =>{
+        User.findOne({email:email})
+        .then(user => { 
+            if (!user) {const newUser = new User({email,password});
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err,hash)=>{
+                    if (err) throw err;
+                    newUser.password = hash;
+                    newUser
+                        .save()
+                        .then(user =>{
+                            return done(null,user);
+                        })
+                        .catch(er => {
+                            return done(null,false, {message:err})
+                        })
+                })
+            })
+        }
+            else{
+                bcrypt.compare(password, user.password, (err,isMatch)=>{
+                    if (err) throw err;
+                    if (is.Match){
+                        return done(null,user);
+                    }
+                    else {
+                        return done(null,false, {message:"Wrong password"})
+                    }
+                })
+            }
+        })
+        .catch(err => {
+            return done(null,false,{message:err})
+        })
+    })
+);
+
+passport.serializeUser((user,done)=> {
+    done(null,user.id);
+});
+
+passport.deserializeUser((id,done) =>{
+    User.findById(id, (err,user)=>{
+        done(err,user);
+    });
+});
+
+module.exports = passport;
