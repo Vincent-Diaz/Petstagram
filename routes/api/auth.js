51077b018e3c74bd2d957e911708f9ab8e4a4c3c
Dung Nguyen <dudu.nt107@gmail.com>
2020-12-03 00:47:38 -0600

    changed app to router

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,18 +1,20 @@
 // Requiring our models and passport as we've configured it
 const db = require("../../models");
 const passport = require("../../config/passport");
+const router = require('express').Router;
 
-module.exports = function(app) {
+module.exports = function() {
   // Route for logging in
-  app.post("/api/login", passport.authenticate("local"), function(req, res) {
+  router.post("/api/login", passport.authenticate("local"), function(req, res) {
     res.json(req.user);
   });
 
   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
-  app.post("/api/signup", function(req, res) {
+  router.post("/api/signup", function(req, res) {
     db.User.create({
+      userName: req.body.userName,
       email: req.body.email,
       password: req.body.password
     })
@@ -25,13 +27,13 @@ module.exports = function(app) {
   });
 
   // Route for logging user out
-  app.get("/logout", function(req, res) {
+  router.get("/logout", function(req, res) {
     req.logout();
     res.redirect("/");
   });
 
   // Route for getting some data about our user to be used client side
-  app.get("/api/user_data", function(req, res) {
+  router.get("/api/user_data", function(req, res) {
     if (!req.user) {
       // The user is not logged in, send back an empty object
       res.status(401).json({});
