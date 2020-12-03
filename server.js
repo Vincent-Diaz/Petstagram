15c059e70efc9fc718fb99b807687faa482adaf9
Dung Nguyen <dudu.nt107@gmail.com>
2020-12-03 11:14:08 -0600

    updated

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3001;
 require('dotenv').config();
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
