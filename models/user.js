fdb34c09f9769ecf580b111ab6a0e58295070061
Dung Nguyen <dudu.nt107@gmail.com>
2020-12-02 21:35:32 -0600

    added third party model

--- models/user.js
+++ models/user.js
@@ -1,13 +1,29 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-  username: { 
-    type: String, 
-    required: true,
-    trim:true,
-    unique:true
-    },
+const ThirdPartyProviderSchema = new Schema({
+  provider_name: {
+    type: String,
+    default:null
+  }, 
+  provider_id:{
+    type:String,
+    default:null
+  },
+  provider_data:{
+    type:{},
+    default:null
+  }
+});
+
+const UserSchema = new Schema(
+  {
+    username: { 
+      type: String, 
+      required: true,
+      trim:true,
+      unique:true
+      },
     email: { 
         type: String, 
         required: true,
@@ -20,6 +36,22 @@ const UserSchema = new Schema({
         trim:true,
         validate: [({ length }) => length >= 6, "Password should be longer."]
     },
+    referral_code:{
+      type:String,
+      default: function(){
+        let hash=0;
+        for (let i=0; i< this.email.length; i++){
+          hash = this.email.charCodeAt(i) + ((hash<<5)-hash);        
+        }
+        let res = (hash & 0x00ffffff).toString(16).toUpperCase();
+        return "00000".substring(0, 6 - res.length) + res;
+      }
+    },
+    referred_by:{
+      type:String,
+      default:null
+    },
+    third_party_auth: [ThirdPartyProviderSchema],
     userCreated: { 
         type: Date, 
         default: Date.now 
@@ -30,7 +62,9 @@ const UserSchema = new Schema({
           ref: "Post"
         }
       ]
-});
+  },
+  {strict:false}
+);
 
 const User = mongoose.model("User", UserSchema);
 
