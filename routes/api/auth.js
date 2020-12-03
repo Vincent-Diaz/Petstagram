8905648adf7bfdf6e2c1ca0064c09dfc54a96a4c
Dung Nguyen <dudu.nt107@gmail.com>
2020-12-03 00:16:43 -0600

    added more routes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,24 +1,47 @@
-const passport = require("passport");
-const express = require("express");
-const router = express.Router();
+// Requiring our models and passport as we've configured it
+const db = require("../../models");
+const passport = require("../../config/passport");
 
-router.post("/login", (req,res,next) =>{
-  passport.authenticate("local", function(err,user,info){
-    if (err){
-      return res.status(400).json({errors:err});
-    }
-    if(!user){
-      return res.status(400).json({errors:"No user found"});
-    }
-    req.logIn(user, function(err){
-      if (err){
-        return res.status(400).json({errors:err});
-      }
-      return res.status(200).json({success:`logged in ${user.id}`})
+module.exports = function(app) {
+  // Route for logging in
+  app.post("/api/login", passport.authenticate("local"), function(req, res) {
+    res.json(req.user);
+  });
+
+  // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
+  // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
+  // otherwise send back an error
+  app.post("/api/signup", function(req, res) {
+    db.User.create({
+      email: req.body.email,
+      password: req.body.password
     })
-  })
-  (req,res,next);
-});
+      .then(function() {
+        res.redirect(307, "/api/login");
+      })
+      .catch(function(err) {
+        res.status(401).json(err);
+      });
+  });
 
+  // Route for logging user out
+  app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/");
+  });
 
-module.exports = router;
+  // Route for getting some data about our user to be used client side
+  app.get("/api/user_data", function(req, res) {
+    if (!req.user) {
+      // The user is not logged in, send back an empty object
+      res.status(401).json({});
+    } else {
+      // Otherwise send back the user's email and id
+      // Sending back a password, even a hashed password, isn't a good idea
+      res.json({
+        email: req.user.email,
+        id: req.user.id
+      });
+    }
+  });
+};
