0332ed64293c37ce425e3131ff66ca6774d8c336
Vincent Diaz <Vincent-Diaz@users.noreply.github.com>
2020-12-04 15:11:27 -0600

    added sortrd method

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -3,8 +3,8 @@ const db = require("../models")
 module.exports = {
     findAll: function (req, res) {
         db.Post
-            .find(req.query)
-            .sort({ date: -1 })
+            .find({})
+            .sort({ date: +1 })
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
