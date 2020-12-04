97a29696e79bf5f534b0e5a4e68689be6d1520d5
Vincent Diaz <Vincent-Diaz@users.noreply.github.com>
2020-12-03 21:57:54 -0600

    added form group for upload file

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -10,10 +10,23 @@ function Home() {
     const post2 = testPost[2]
     return (
         <>
-            <Navigation {...post0} />
-           <Post {...post0}/> 
-           <Post {...post1}/>
-           <Post {...post2}/>
+            <div>
+                <Navigation {...post0} />
+                <form>
+                    <div className="form-group">
+                        <label htmlFor="image">Image</label>
+                        <input type="file" className="form-control-file" id="exampleFormControlFile1" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="caption">Caption</label>
+                        <input type="text" className="form-control" id="exampleInputPassword1" />
+                    </div>
+                    <button type="submit" className="btn btn-primary" id="uploadBtn">Upload</button>
+                </form>
+                <Post {...post0} />
+                <Post {...post1} />
+                <Post {...post2} />
+            </div>
         </>
     );
 }
