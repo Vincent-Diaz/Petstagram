aa9d5b4e56f7b0e9cf3d7d1ab66867e9dc898ad0
Dung Nguyen <dudu.nt107@gmail.com>
2020-12-04 21:44:36 -0600

    added api routes to find friends- testing-stage

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const imgRoutes = require("./img");
 const authRoutes = require("./auth");
+const userRoutes = require("./user");
 
 router.use("/img", imgRoutes);
 router.use(authRoutes);
+// router.use(userRoutes)
 
 module.exports = router;
---
+++ routes/api/user.js
@@ -0,0 +1,6 @@
+// const router = require("express").Router();
+// const usersControllers = require("../../controllers/usersControllers");
+
+// router.route("/")
+//   .get(usersControllers.findByUserName);
+// module.exports = router;
