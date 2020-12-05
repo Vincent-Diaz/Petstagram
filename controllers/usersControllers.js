e1be8b12ffb329dda19b09aaa0585338e9eda8e0
Dung Nguyen <dudu.nt107@gmail.com>
2020-12-04 21:43:43 -0600

    added usersControllers- testing-stage

---
+++ controllers/usersControllers.js
@@ -0,0 +1,11 @@
+// const db = require("../models")
+
+// module.exports = {
+//     findByUserName: function (req, res) {
+//         console.log("uers",req)
+//         // db.User
+//         //     .findOne({userName:req})
+//         //     .then(dbModel => res.json(dbModel))
+//         //     .catch(err => res.status(422).json(err));
+//     }
+// };
