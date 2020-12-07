277fe8d6f575e17eed944cacd722e36c5f9400ed
Vincent Diaz <Vincent-Diaz@users.noreply.github.com>
2020-12-07 14:57:19 -0600

    added avatar to page and used alt userName

--- client/src/components/Post/Post.js
+++ client/src/components/Post/Post.js
@@ -2,21 +2,22 @@ import React from 'react';
 import './Post.css';
 import Image from 'react-bootstrap/Image'
 import Container from 'react-bootstrap/Container'
+import Avatar from '@material-ui/core/Avatar';
 
 function Post( props ) {
     return (
         <Container style={{marginLeft: "100px"}}>
-        <div className="post">
+        <div className="post" key={props._id}>
                 <div className="post__header">
-                <Image className="post__avatar" src={props.profilePic}/>
-                <h3><a href="/profile">{props.username}</a></h3>
+                <Avatar className="post__avatar" alt={props.title} src="/static/images/avatar/1.jpg"/>
+                <h3><a href="/profile">{props.title}</a></h3>
             </div>
            
             {/* header -> avater + username */}
 
             <Image className="post__image" src={props.imageUrl} />
 
-            <h4 className="post__text"><strong><a href="/profile">{props.username}</a></strong> {props.caption}</h4>
+            <h4 className="post__text"><strong><a href="/profile">{props.title}</a></strong> {props.caption}</h4>
         </div>
         </Container>
     )
