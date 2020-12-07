4d14dd4d24667979f5312ee9051340f99357f796
Zinc <greenberg.zachary@utexas.edu>
2020-12-07 17:44:26 -0600

    uploads are printing cards

--- client/src/components/Post/Post.js
+++ client/src/components/Post/Post.js
@@ -6,7 +6,7 @@ import Avatar from '@material-ui/core/Avatar';
 
 function Post(props) {
     return (
-        <div className="post card home-card" key={props._id}>
+        <div className="card home-card" key={props._id}>
             <div className="post__header">
                 <Avatar className="post__avatar" alt={props.title} src="/static/images/avatar/1.jpg" />
                 <h3><a href="/profile">{props.title}</a></h3>
@@ -14,11 +14,8 @@ function Post(props) {
             <div className="card-image">
                 <img src={props.imageUrl} />
             </div>
-
-            {/* <Image className="post__image" src={props.imageUrl} /> */}
             <div className="card-content">
-                <i className="material-icons" style={{ color: "red" }}>favorite</i>
-                <h4>{props.title}</h4>
+                {/* <i className="material-icons" style={{ color: "red" }}>favorite</i> */}
                 <p>{props.caption}</p>
                 <input type="text" placeholder="add a comment" />
             </div>
