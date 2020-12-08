43d49b8b518673fa04ed7377d4dbc09cea1be130
Dung Nguyen <dudu.nt107@gmail.com>
2020-12-07 23:08:32 -0600

    added search for user feature

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -4,7 +4,7 @@ module.exports = {
     findUserByUserName: function (req, res) {
         // console.log("server side user",req.params.username)
         db.User
-            .findOne({userName:req.params.userName})
+            .find({userName:new RegExp('^'+req.params.username+'.*$', "i")})
             .then(dbModel => {
                 res.json(dbModel);
                 // console.log("user found", dbModel)
